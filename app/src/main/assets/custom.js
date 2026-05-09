90b4db5478a2ca93e8285aa2ac38b875d2fbc0b5
zwswin <zwswin@qq.com>
2026-05-09 11:53:29 +0800

    update custom js from pakeplus

---
+++ app/src/main/assets/custom.js
@@ -0,0 +1,20 @@
+window.addEventListener("DOMContentLoaded",()=>{const t=document.createElement("script");t.src="https://www.googletagmanager.com/gtag/js?id=G-W5GKHM0893",t.async=!0,document.head.appendChild(t);const n=document.createElement("script");n.textContent="window.dataLayer = window.dataLayer || [];function gtag(){dataLayer.push(arguments);}gtag('js', new Date());gtag('config', 'G-W5GKHM0893');",document.body.appendChild(n)});const { invoke } = window.__TAURI__.core
+const hookClick = (e) => {
+const origin = e.target.closest('a')
+const isBaseTargetBlank = document.querySelector(
+    'head base[target="_blank"]'
+)
+if (
+    (origin && origin.href && origin.target === '_blank') ||
+    (origin && origin.href && isBaseTargetBlank)
+) {
+    e.preventDefault()
+    invoke('open_url', { url: origin.href })
+}
+}
+
+window.open = function (url, target, features) {
+invoke('open_url', { url: url })
+}
+
+document.addEventListener('click', hookClick, { capture: true })
